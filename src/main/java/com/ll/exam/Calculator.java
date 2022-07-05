913e2dd401fbebe9fdb02376ca3f6e4321768b94
seeunla <ksu98712@gmail.com>
2022-07-05 14:02:29 +0900

    더하기 기능 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -2,9 +2,9 @@ package com.ll.exam;
 
 public class Calculator {
     public static int add(String s) {
-        if ( s.equals("10+20")) {
-            return 30;
-        }
-        return 40;
+        String[] sBits = s.split(" \\+ ");
+        int no1 = Integer.parseInt(sBits[0]); // '1'
+        int no2 = Integer.parseInt(sBits[1]);
+        return no1 + no2;
     }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -16,4 +16,11 @@ public class AppTest {
         int rs = Calculator.add("20 + 20");
         assertEquals(40,rs);
     }
+
+    @Test // 컴퓨터가 읽을 수 있게 해준다.
+    public void 더하기_테스트3() {
+        int rs = Calculator.add("10 + 10");
+
+        assertEquals(20,rs);
+    }
 }
